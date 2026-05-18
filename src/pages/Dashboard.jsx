10a16492c5eaf96165aecd1de58240e40574eb74
Omar Mauldin <w0650036@myscc.ca>
2026-05-18 16:33:51 -0400

    chore: convert project config to JavaScript

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router";
+
 export default function Dashboard() {
   return (
     <div>
       <h1 className="text-2xl font-semibold">Dashboard</h1>
       <p className="text-sm text-zinc-400 mt-2">Track today’s workout, progress, and weekly performance.</p>
+      <Link to="/workouts">goto Workouts</Link>
     </div>
   )
 }
